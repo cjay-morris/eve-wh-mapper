f1f52f0552394d016bc69b6c02777636fd97114c
UP2009271 <91533218+UP2009271@users.noreply.github.com>
2024-11-24 02:54:58 +0000

    fix: re-add auth sign in and out

--- src/app/ns/page.tsx
+++ src/app/ns/page.tsx
@@ -3,8 +3,10 @@
 import SignatureList from '~/components/SignatureList'
 import SignatureInput from '~/components/SignatureInput'
 import { useChainContext } from "~/context/ChainContextProvider"
+import { signOut, signIn, useSession } from 'next-auth/react'
 
 const HomePage = () => {
+  const { data: session } = useSession()
   const { currentLocation } = useChainContext()
 
   return (
@@ -13,8 +15,11 @@ const HomePage = () => {
       You are currently at: {currentLocation}
       <SignatureInput />
       <SignatureList />
+      <button className="p-2 bg-blue-500 text-white rounded" onClick={() => session ? signOut() : signIn()}>
+        Sign { session ? 'out' : 'in' }
+      </button>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
